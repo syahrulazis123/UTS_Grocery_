b4b5c2d6ceb1450f0aee50799997ba6b422c2abc
syahrul <syahrulazis0706@.com>
2023-06-15 20:43:07 +0700

    first commit

---
+++ lib/constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const blue = Color(0xFF102A68);
+const white = Color(0xFFF8FAFF);
+const darkorange = Color.fromARGB(255, 0, 212, 46);
+const orange = Color.fromARGB(255, 3, 189, 43);
+const grey = Colors.grey;
+final poppins = GoogleFonts.poppins();
